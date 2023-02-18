b36c87373f2fcd23e70940c864949915de32b484
JakeVigeantProfessional <jmv43duke.edu>
2023-02-17 20:26:13 -0500

    typo

--- multdiv.v
+++ multdiv.v
@@ -1,34 +1,3 @@
-// module multdiv(
-// 	data_operandA, data_operandB, 
-// 	ctrl_MULT, ctrl_DIV, 
-// 	clock, 
-// 	data_result, data_exception, data_resultRDY);
-
-//     input [31:0] data_operandA, data_operandB;
-//     input ctrl_MULT, ctrl_DIV, clock;
-
-//     output [31:0] data_result;
-//     output data_exception, data_resultRDY;
-
-//     wire either, currentMult, currentDiv, currentMultAndMultRdy, currentDivAndDivRdy;
-//     or(either, ctrl_MULT, ctrl_DIV);
-//     dffe_ref saveMult(currentMult, ctrl_MULT, clock, either, 1'b0);
-//     dffe_ref saveDiv(currentDiv, ctrl_DIV, clock, either, 1'b0);
-
-    
-//     wire[31:0] multRes, divRes;
-//     wire multException, divException, multRdy, divRdy;
-
-//     mult multiply(data_operandA, data_operandB, clock, ctrl_MULT, multRes, multException, multRdy);
-//     // div divide(data_operandA, data_operandB, clock, ctrl_DIV, divRes, divException, divRdy);
-    
-//     assign currentMultAndMultRdy = currentMult ? multRdy : 1'b0;
-//     assign currentDivAndDivRdy = currentDiv ? divRdy : 1'b0;
-
-//     mux_2_32bit selectRes(data_result, currentDiv, multRes, divRes);
-//     mux_2 selectException(data_exception, currentDiv, multException, divException);
-//     mux_2 selectReady(data_resultRDY, currentDiv, currentMultAndMultRdy, currentDivAndDivRdy);
-// endmodule
 
 module multdiv(
 	data_operandA, data_operandB,
